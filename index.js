678e58f0a83108cad96904179592e223ec0a10c2
Sukma Suryaningsih <ingsih19@gmail.com>
2022-05-29 21:18:40 +0800

    add logic to fill the null variabel

--- index.js
+++ index.js
@@ -23,19 +23,36 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+
+  for(let i = 0; i < dataYangAkanDilooping.length; i++) {
+    let namaLengkap = dataYangAkanDilooping[i].namaDepan + ' ' + (dataYangAkanDilooping[i].namaBelakang);
+    hasilLooping.push(namaLengkap);
+  };
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
+
+  for(let ip = 0; ip < dataYangAkanDilooping.length; ip++) {
+    if(dataYangAkanDilooping[ip].jenisKelamin == 'M') {
+      jumlahPria++
+    }
+  };
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
+
+  for(let iw = 0; iw < dataYangAkanDilooping.length; iw++) {
+    if(dataYangAkanDilooping[iw].jenisKelamin == 'F') {
+      jumlahWanita++
+    }
+  };
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,7 +65,15 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+
+  if(jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita"
+  } else if(jumlahPria < jumlahWanita) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria"
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang"
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
@@ -65,6 +90,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar)
 
   return hasil;
 }
